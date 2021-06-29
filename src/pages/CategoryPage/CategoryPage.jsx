10f63d815324f0faafac8605a13e7cb98c0c380c
adiletturdakunov2002@gmail.com <ilyas.yangurazov.iy@gmail.com>
2021-06-29 21:56:32 +0600

    fixed: logic of branches

--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { Category } from '../../components/Category/Category'
 import { Header } from '../../components/Header/Header'
+import { Item } from '../../components/Item/Item'
 import { SubCategoryPage } from '../SubCategoryPage/SubCategoryPage'
 import Styles from './CategoryPage.module.css'
 
@@ -19,6 +20,24 @@ export const CategoryPage = ({ language, data }) => {
                 }
             />
             {
+                categoryData.subCategoryStatus === false && categoryData.items.length > 0 ?
+                <div className="container">
+                    <div className={Styles.block}>
+                        {
+                            categoryData.items.map((el, i) => {
+                                return (
+                                    <Item
+                                        language={language}
+                                        key={i}
+                                        data={el}
+                                        categoryId={categoryId}
+                                        i={ i }
+                                    />
+                                )
+                            })
+                        }
+                    </div>
+                </div> :
                 categoryData.subCategoryStatus === false ?
                 <div className="container">
                     <div className={Styles.block}>
